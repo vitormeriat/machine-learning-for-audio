7923940a8532d81491fbaeacbe43f182eff39eae
vitormeriat <vitormeriat@gmail.com>
2022-01-06 16:14:06 +0000

    Update feature extraction

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,14 +1,10 @@
 # Load various imports
 import os
-#import pickle
-#from librosa import feature
 import pandas as pd
 from tqdm import tqdm
-#from helper import extract_features
 import helper as helper
 
 
-# if os.path.exists('../data/features_df'):
 if os.path.isfile('./data/features_df.pkl'):
     print("[INFO] File exist")
 else:
@@ -16,8 +12,6 @@ else:
     #fulldatasetpath = '/Urban Sound/UrbanSound8K/audio/'
     fulldatasetpath = 'C:/research/mvpconf/urbansound8k/'
 
-    #metadata = pd.read_csv(fulldatasetpath + '../metadata/UrbanSound8K.csv')
-    #metadata = pd.read_csv(fulldatasetpath + 'UrbanSound8K.csv')
     metadata = helper.create_metadata_df(fulldatasetpath + 'UrbanSound8K.csv')
     print(metadata.head())
     print('\n[INFO] Start feature extraction\n')
@@ -38,9 +32,7 @@ else:
     print('\n[INFO] Finished feature extraction from ',
           len(features_df), ' files')
 
-    # with open('../data/features_df', 'wb') as picklefile:
-    #    #pickle the dataframe
-    #    pickle.dump(features_df, picklefile)
+    #pickle the dataframe
     features_df.to_pickle('./data/features_df.pkl')
 
     print('[INFO] Pickle a DataFrame...')
